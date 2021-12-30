fd4377f064795fdbb78ea965610d113cb8722005
Denis Kajdic <Denis.Kajdic@cognizant-mobility.com>
2021-12-30 12:08:13 +0100

    LocalUserTransaction tests

--- core/src/test/java/org/openmdx/base/accessor/rest/spi/LocalUserTransactionAdaptersTest.java
+++ core/src/test/java/org/openmdx/base/accessor/rest/spi/LocalUserTransactionAdaptersTest.java
@@ -2,14 +2,18 @@ package org.openmdx.base.accessor.rest.spi;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import javax.naming.NamingException;
+import javax.naming.spi.NamingManager;
 import javax.resource.ResourceException;
 
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openmdx.application.transaction.JTALocalUserTransactionAdapter;
+import org.junit.jupiter.api.function.Executable;
 import org.openmdx.base.transaction.LocalUserTransaction;
+import org.openmdx.kernel.lightweight.naming.NonManagedInitialContextFactoryBuilder;
 import org.openmdx.kernel.loading.Factory;
 import org.openmdx.kernel.platform.Platform;
 
@@ -18,12 +22,16 @@ class LocalUserTransactionAdaptersTest {
     Factory<LocalUserTransaction> jtaUserTransactionFactory;
     Factory<LocalUserTransaction> containerManagedUserTransactionFactory;
 
-    private LocalUserTransactionAdapters underTest;
+    private static LocalUserTransactionAdapters underTest;
     private String jtaKey;
     private String containerManagedKey;
 
-    //    PersistenceManagerFactory pmf;
-//    private PersistenceManager pm;
+    @BeforeAll
+    public static void jndiSetUp() throws NamingException {
+        if (!NamingManager.hasInitialContextFactoryBuilder()) {
+            NonManagedInitialContextFactoryBuilder.install(null);
+        }
+    }
 
     @BeforeEach
     void setUp() {
@@ -37,37 +45,35 @@ class LocalUserTransactionAdaptersTest {
         );
     }
 
-    @AfterEach
-    void tearDown() {
-    }
-
-//    @Test
-//    @Ignore
-//    void getResourceLocalUserTransactionAdapter() throws ResourceException {
-//
-//
-//
-//    }
-
     @Test
-    void getJTAUserTransactionAdapter() throws ResourceException {
+    void testGetJTAUserTransactionAdapter() throws ResourceException {
+        final String className = jtaUserTransactionFactory.getInstanceClass().getName();
         final LocalUserTransaction localUserTransaction =
                 LocalUserTransactionAdapters.getJTAUserTransactionAdapter();
 
+        assertEquals(Platform.getProperty(jtaKey), className);
         assertNotNull(localUserTransaction);
-        assertEquals(Platform.getProperty(jtaKey), jtaKey);
-
     }
 
     @Test
-    void getContainerManagedUserTransactionAdapter() throws ResourceException {
+    void testGetJTAUserTransactionAdapterPlatformKeyNotExists() {
+        jtaUserTransactionFactory = new LocalUserTransactionFactory(Platform.getProperty("non.existent.platform.key"));
+        assertThrows(NullPointerException.class, (Executable) underTest);
+    }
 
-        final LocalUserTransaction localUserTransaction =
-                LocalUserTransactionAdapters.getContainerManagedUserTransactionAdapter();
-
+    @Test
+    void testGetContainerManagedUserTransactionAdapter() throws ResourceException {
         final String className = containerManagedUserTransactionFactory.getInstanceClass().getName();
+        final LocalUserTransaction localUserTransaction =
+                underTest.getContainerManagedUserTransactionAdapter();
 
-        assertNotNull(localUserTransaction);
         assertEquals(Platform.getProperty(containerManagedKey), className);
+        assertNotNull(localUserTransaction);
+    }
+
+    @Test
+    void testGetContainerManagedUserTransactionAdapterPlatformKeyNotExists() {
+        containerManagedUserTransactionFactory = new LocalUserTransactionFactory(Platform.getProperty("non.existent.platform.key"));
+        assertThrows(NullPointerException.class, (Executable) underTest);
     }
 }
