39e2494be1b1e02bcf9e1b6cb547508e0e50a3a2
Dirty Harry <dirty-harry@ikmail.com>
2025-05-31 01:11:08 +0200

    MDX-1 change Bootstrap Version 3.20.0

--- openmdx-3-platform/build.gradle.kts
+++ openmdx-3-platform/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     constraints {
     	
 		val atomikosVersion = "6.0.0"
-		val bootstrapVersion = "2.19.5" // 3.19.5 isn't released yet
+		val bootstrapVersion = "3.20.0"
 		val cacheVersion = "1.1.1"
 		val flexmarkVersion = "0.62.2"
 		val groovyVersion = "3.0.+"
